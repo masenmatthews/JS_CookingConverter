ec8b24900708288acb05e04e2b05418f2e281b55
Jared Clemmensen and Masen Matthews <jared+masen@>
2018-01-08 10:52:01 -0800

    write functions for a few measurement conversions.

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+var galToLiter = function(gallons) {
+  var liters = gallons * 3.79;
+  return liters;
+};
+
+var ozToTbsp = function(oz) {
+  var tbsp = oz * 2;
+  return tbsp;
+};
+
+var ozToCup = function(oz) {
+  var cup = oz / 8;
+  return cup;
+};
+
+var ozToTsp = function(oz) {
+  var tsp = oz * 6;
+  return tsp;
+};
+
+var ozToQuart = function(oz) {
+  var quart = oz / 32;
+  return quart;
+};
